5924b20d01caf3fb253aa1d827445c1149c19fea
Sohteum <thtwk1123@gmail.com>
2023-10-30 18:57:07 +0900

    Update PokeList.tsx

--- src/components/PokeList.tsx
+++ src/components/PokeList.tsx
@@ -21,12 +21,12 @@ const PokeList = ({ url, isLast }: { url: string, isLast: boolean }) => {
         axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}/`).then((res) => {
           name = res.data.names[2].name;
 
-          // const type = response.data.types.map([[1],[2]]).type.name;
+          const type = response.data.types.map((types,idx)=>{types}).type.name;
           setPokemon({
             id,
             name: name,
-            // type,
-            type: response.data.types[0].type.name, //맵을 돌릴수있나아
+            type,
+            // type: response.data.types[0].type.name, //맵을 돌릴수있나아
             image: response.data.sprites.other["official-artwork"]["front_default"],
             height: response.data.height,
             weight: response.data.weight,
